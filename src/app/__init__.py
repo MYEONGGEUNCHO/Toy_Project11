c9a1417a962d6d9eef460c6c4a97c3e4f0a04b65
mgcho <mgcho@egap.co.kr>
2023-03-14 08:53:27 +0900

    debug settings

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -36,13 +36,13 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
     
-    from . import db
+    import db
     db.init_app(app)
     
-    from . import auth
+    import auth
     app.register_blueprint(auth.bp)
     
-    from . import blog
+    import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
